a7568ca311294dd87674256ddd84880fdda77e48
alansantos <alanpontoinfo@gmail.com>
2021-05-17 18:09:54 -0300

    Add app.get json for sen object json

--- myApp.js
+++ myApp.js
@@ -13,7 +13,9 @@ res.sendFile(__dirname + "/views/index.html");
 app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
 
-
+app.get("/json", function(req, res){
+    res.json({"message":"Hello json"})
+})
 
 
 
